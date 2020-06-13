be1e0571fbc5700c97bb67877de588e5e44e7eef
Einjerjar <erjill87@gmail.com>
2020-06-14 07:51:38 +0800

    fixed file sorting issue

--- img2Pdf.py
+++ img2Pdf.py
@@ -7,30 +7,40 @@ d = {'quality': 90, 'optimize': True}
 
 
 def genPDF(source, quality=90, optimize=True, out=None):
+    # * Allowed file extensions
     imgExts = ['jpg', 'jpeg', 'png', 'webp', 'tiff']
+    # * File list
     imgSources = []
 
+    # TODO #1 optimize
     for i in imgExts:
         imgSources.extend(glob.glob(os.path.join(source, '*.{}'.format(i))))
 
+    # * Sort images (will be removed when TODO #1 is done)
+    imgSources.sort()
+
+    # * Check for filecount, abort if 0
     print('Found {} files'.format(len(imgSources)))
     if (len(imgSources) == 0):
         print('No files found, aborting')
         return
 
+    # * Pillow images
     imgList = []
 
+    # * Settings
     quality = quality
     optimize = optimize
-
     outFile = out
 
+    # * Make sure an outfile is available
     if outFile is None:
         outFile = os.path.join(os.path.dirname(imgSources[0]), os.path.splitext(
             os.path.basename(imgSources[0]))[0] + '.pdf')
     elif os.path.splitext(outFile)[1].strip().lower() != '.pdf.':
         outFile = os.path.splitext(outFile)[0] + '.pdf'
 
+    # * Load images to memory
     print('Preparing images')
     for i in imgSources:
         img = Image.open(i)
@@ -38,25 +48,32 @@ def genPDF(source, quality=90, optimize=True, out=None):
 
         imgList.append(img)
 
+    # * Get first image (for saving purposes)
     root = imgList.pop(0)
 
+    # * Save file
     print('Saving to [{}] :: quality: {} :: optimized: {}'. format(
         outFile, quality, optimize))
     root.save(outFile, save_all=True, append_images=imgList,
               optimize=optimize, quality=quality)
 
+    # * Done
     print('Saved pdf as [{}]'.format(outFile))
 
 
-def s2b(v):
+def s2b(v, default=True):
+    '''
+    Checks if a string can be converted to a boolean
+    '''
     if isinstance(v, bool):
         return v
     elif v.lower() in ['f', 'n', '0', 'false']:
         return False
-    return True
+    return default
 
 
 if __name__ == "__main__":
+    # * Only import if ran directly
     import argparse
 
     parser = argparse.ArgumentParser(
@@ -72,4 +89,5 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    # * Do magic
     genPDF(args.source, args.quality, args.optimize, args.out)
