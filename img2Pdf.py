3368e9c8f28528cbf0f3f7aa8fa6904cf1be3791
Einjerjar <erjill87@gmail.com>
2020-06-14 08:44:06 +0800

    refactored stuff to abide snake_case format for the functions and vars

--- img2Pdf.py
+++ img2Pdf.py
@@ -6,65 +6,63 @@ from PIL import Image
 d = {'quality': 90, 'optimize': True}
 
 
-def genPDF(source, quality=90, optimize=True, out=None):
+def gen_pdf(source, quality=90, optimize=True, out=None):
     # * Allowed file extensions
-    imgExts = ['jpg', 'jpeg', 'png', 'webp', 'tiff']
+    img_exts = ['jpg', 'jpeg', 'png', 'webp', 'tiff']
     # * File list
-    imgSources = []
+    img_sources = []
 
     # TODO #1 optimize
-    for i in imgExts:
-        imgSources.extend(glob.glob(os.path.join(source, '*.{}'.format(i))))
+    for i in img_exts:
+        img_sources.extend(glob.glob(os.path.join(source, '*.{}'.format(i))))
 
     # * Sort images (will be removed when TODO #1 is done)
-    imgSources.sort()
+    img_sources.sort()
 
     # * Check for filecount, abort if 0
-    print('Found {} files'.format(len(imgSources)))
-    if (len(imgSources) == 0):
+    print('Found {} files'.format(len(img_sources)))
+    if len(img_sources) == 0:
         print('No files found, aborting')
         return
 
     # * Pillow images
-    imgList = []
+    img_list = []
 
     # * Settings
     quality = quality
     optimize = optimize
-    outFile = out
+    out_file = out
 
     # * Make sure an outfile is available
-    if outFile is None:
-        outFile = os.path.join(os.path.dirname(imgSources[0]), os.path.splitext(
-            os.path.basename(imgSources[0]))[0] + '.pdf')
-    elif os.path.splitext(outFile)[1].strip().lower() != '.pdf.':
-        outFile = os.path.splitext(outFile)[0] + '.pdf'
+    if out_file is None:
+        out_file = os.path.join(os.path.dirname(img_sources[0]), os.path.splitext(
+            os.path.basename(img_sources[0]))[0] + '.pdf')
+    elif os.path.splitext(out_file)[1].strip().lower() != '.pdf.':
+        out_file = os.path.splitext(out_file)[0] + '.pdf'
 
     # * Load images to memory
     print('Preparing images')
-    for i in imgSources:
+    for i in img_sources:
         img = Image.open(i)
         img.convert('RGB')
 
-        imgList.append(img)
+        img_list.append(img)
 
     # * Get first image (for saving purposes)
-    root = imgList.pop(0)
+    root = img_list.pop(0)
 
     # * Save file
-    print('Saving to [{}] :: quality: {} :: optimized: {}'. format(
-        outFile, quality, optimize))
-    root.save(outFile, save_all=True, append_images=imgList,
-              optimize=optimize, quality=quality)
+    print('Saving to [{}] :: quality: {} :: optimized: {}'.format(out_file, quality, optimize))
+    root.save(out_file, save_all=True, append_images=img_list, optimize=optimize, quality=quality)
 
     # * Done
-    print('Saved pdf as [{}]'.format(outFile))
+    print('Saved pdf as [{}]'.format(out_file))
 
 
 def s2b(v, default=True):
-    '''
+    """
     Checks if a string can be converted to a boolean
-    '''
+    """
     if isinstance(v, bool):
         return v
     elif v.lower() in ['f', 'n', '0', 'false']:
@@ -85,9 +83,9 @@ if __name__ == "__main__":
     parser.add_argument('-q', '--quality', metavar='INT', type=int, default=d['quality'],
                         help='the final output quality [default: {}]'.format(d['quality']))
     parser.add_argument('-p', '--optimize', metavar='BOOL', type=s2b, default=d['optimize'],
-                        help='wether to try and optimize the images [default: {}]'.format(d['optimize']))
+                        help='whether to try and optimize the images [default: {}]'.format(d['optimize']))
 
     args = parser.parse_args()
 
     # * Do magic
-    genPDF(args.source, args.quality, args.optimize, args.out)
+    gen_pdf(args.source, args.quality, args.optimize, args.out)
--- img2PdfGui.py
+++ img2PdfGui.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, Frame, StringVar, BooleanVar, Label, Entry, Button, Checkbutton, filedialog
-from img2Pdf import genPDF
+from img2Pdf import gen_pdf
 
 
 class ITP(Frame):
@@ -7,27 +7,8 @@ class ITP(Frame):
         Frame.__init__(self, master)
         self.master = master
 
-        self.init()
-        self.pack()
-
-    def genPDF(self, e=None):
-        o = self.outFolder.get()
-        print(o, len(o))
-        oo = None if len(o.strip()) == 0 else o
-        genPDF(self.sourceFolder.get(), int(
-            self.quality.get()), self.optimize.get(), oo)
-
-    def getSourceDir(self):
-        self.sourceFolder.set(filedialog.askdirectory(
-            title='Select folder containing the images', initialdir='./'))
-
-    def getOutFile(self):
-        self.outFolder.set(filedialog.asksaveasfilename(
-            title='Save to pdf', initialdir='./'))
-
-    def init(self):
-        self.sourceFolder = StringVar()
-        self.outFolder = StringVar()
+        self.source_folder = StringVar()
+        self.out_folder = StringVar()
 
         self.quality = StringVar()
         self.quality.set('90')
@@ -35,32 +16,52 @@ class ITP(Frame):
         self.optimize = BooleanVar()
         self.optimize.set(True)
 
+        self.init()
+        self.pack()
+
+    def gen_pdf(self):
+        o = self.out_folder.get()
+        print(o, len(o))
+        oo = None if len(o.strip()) == 0 else o
+        gen_pdf(self.source_folder.get(), int(
+            self.quality.get()), self.optimize.get(), oo)
+
+    def get_source_dir(self):
+        self.source_folder.set(filedialog.askdirectory(
+            title='Select folder containing the images', initialdir='./'))
+
+    def get_out_file(self):
+        self.out_folder.set(filedialog.asksaveasfilename(
+            title='Save to pdf', initialdir='./'))
+
+    def init(self):
+
         Label(self, text='Folder root:').grid(row=0, column=0)
-        sf = Entry(self, textvariable=self.sourceFolder)
-        sf.bind('<Return>', self.genPDF)
+        sf = Entry(self, textvariable=self.source_folder)
+        sf.bind('<Return>', self.gen_pdf)
         sf.grid(row=0, column=1, columnspan=2, sticky='EW')
         sf.focus_set()
 
-        Button(self, text='...', command=self.getSourceDir).grid(row=0, column=3)
+        Button(self, text='...', command=self.get_source_dir).grid(row=0, column=3)
 
         Label(self, text='Output folder:').grid(row=1, column=0)
-        of = Entry(self, textvariable=self.outFolder)
-        of.bind('<Return>', self.genPDF)
+        of = Entry(self, textvariable=self.out_folder)
+        of.bind('<Return>', self.gen_pdf)
         of.grid(row=1, column=1, columnspan=2, sticky='EW')
 
-        Button(self, text='...', command=self.getOutFile).grid(row=1, column=3)
+        Button(self, text='...', command=self.get_out_file).grid(row=1, column=3)
 
         Label(self, text='Quality').grid(row=2, column=0)
         qf = Entry(self, textvariable=self.quality)
-        qf.bind('<Return>', self.genPDF)
+        qf.bind('<Return>', self.gen_pdf)
         qf.grid(row=2, column=1)
 
         Label(self, text='Optimize').grid(row=2, column=2)
         pf = Checkbutton(self, variable=self.optimize)
         pf.grid(row=2, column=3)
 
-        gb = Button(self, text='Generate', command=self.genPDF)
-        gb.bind('<Return>', self.genPDF)
+        gb = Button(self, text='Generate', command=self.gen_pdf)
+        gb.bind('<Return>', self.gen_pdf)
         gb.grid(row=3, column=0, columnspan=4, sticky='EW')
 
 
